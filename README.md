f07abd8b6584a882aa812dead908cf5f4edef29b
DiegoHalac <diegohalac@yahoo.com.ar>
2025-04-02 18:43:55 -0300

    pagina

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-"# PRODUCTOS" 
+# Mi Página en GitHub Pages  
+
+Este repositorio es para administrar las descripciones de productos de DHC  
+
+## Contenido  
+- `index.html`: Página principal.  
+- `README.md`: Descripción del proyecto.  
+
+## Enlace  
+Puedes ver la página en: [https://tu_usuario.github.io](https://tu_usuario.github.io)
---
+++ script.js
@@ -0,0 +1,10 @@
+function showContent(contentId) {
+    document.querySelectorAll('.hidden-content').forEach(content => {
+        content.classList.remove('active');
+    });
+
+    document.getElementById(contentId).classList.add('active');
+}
+
+// Mostrar la primera sección por defecto
+//window.onload = () => showContent('hoja-de-ruta');
